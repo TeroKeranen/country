0c4c382210bcdba4743781d6c103eb6ca73cee5a
Tero Keränen <tero-keranen@hotmail.com>
2023-02-08 12:31:32 +0200

    Add project to github repo

---
+++ app.js
@@ -0,0 +1,19 @@
+// require express
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, function () {
+  console.log("server start on port 3000");
+});
